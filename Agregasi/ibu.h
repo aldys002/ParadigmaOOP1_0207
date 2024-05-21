48026f0a97e8be68805499c952c9cc794e9a280a
aldys002 <igidiaaldys@gmail.com>
2024-05-21 15:54:10 +0700

    menambah void cetakanak

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -2,6 +2,18 @@
 #define IBU_H
 
 class ibu {
+public: 
+   string nama;
+   vector<anak*> daftar_anak;
 
+   ibu(string pNama) :nama(pNama) {
+    cout <<"Ibu \""<< nama << "\"ada\n";
+
+   }
+   ~ibu(){
+    cout << "ibu \" "<< nama <<"\" tidak ada\n";
+       }
+   void tambahAnak(*anak);
+   void cetakAnak();
 };
 #endif
